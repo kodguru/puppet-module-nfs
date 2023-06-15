ef5b4198f4be7c4a0c44d988d0ad9129689e60f5
Phil Friderici <phil.friderici@elits.com>
2023-06-15 12:18:02 +0000

    Satisfy puppet-lint, fix tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -104,8 +104,12 @@ describe 'nfs' do
     describe "on osfamily <#{v[:osfamily]}> when #{v[:release].nil? ? 'kernel' : 'operatingsystemmaj'}release is <#{v[:release]}#{v[:kernelrelease]}>" do
       let :facts do
         {
-          osfamily:                  v[:osfamily],
-          operatingsystemmajrelease: v[:release],
+          os: {
+            family:  v[:osfamily],
+            release: {
+              major: v[:release],
+            },
+          },
           kernelrelease:             v[:kernelrelease],
         }
       end
@@ -393,11 +397,15 @@ describe 'nfs' do
   end
 
   unsupported_platforms.sort.each do |_k, v|
-    describe "on unsupported osfamily <#{v[:osfamily]}> when #{v[:release].nil? ? 'kernel' : 'operatingsystemmaj'}release is <#{v[:release]}#{v[:kernelrelease]}>" do
+    describe "on unsupported [os][family] <#{v[:osfamily]}> when #{v[:release].nil? ? 'kernel' : '[os][release][major]'}release is <#{v[:release]}#{v[:kernelrelease]}>" do
       let :facts do
         {
-          osfamily:                  v[:osfamily],
-          operatingsystemmajrelease: v[:release],
+          os: {
+            family:  v[:osfamily],
+            release: {
+              major: v[:release],
+            },
+          },
           kernelrelease:             v[:kernelrelease],
         }
       end
